3f7509d936cff6adb74f1670460b568c35fd5871
Marvin Odobashi <marv.odobashi@gmail.com>
2024-03-13 11:42:01 -0400

    updated makefile; fixed problems with up/down facing sprite; fixed? issue with different speeds for different sized windows

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 {
 
     // make window fullscreen compatible
-    sf::RenderWindow window(sf::VideoMode(800, 600), "test");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Adventures of Narnok");
 
     // match screen's frame rate
     window.setVerticalSyncEnabled(true);
@@ -24,7 +24,7 @@ int main()
     // Ms. Sullivan - nuff said
     // I wonder if there is an easier way to do this? Make it portable - there probably is
     // look into this
-    if(!texture.loadFromFile("Textures\\Narnok.png")){
+    if(!texture.loadFromFile("./Textures/Narnok.png")){
         return 1;
     }
 
@@ -44,6 +44,8 @@ int main()
     0, 1, 2, 3, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 3, 2, 1, 0,
     0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1, 0,
     0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1, 0,
+    0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1, 0,
+    0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1, 0,
     0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1, 0,
     0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
@@ -51,11 +53,11 @@ int main()
     };
 
     const unsigned int mapWidth = 20;
-    const unsigned int mapHeight = 18;
+    const unsigned int mapHeight = 20;
 
 
     TileMap map;
-    if(!map.load("Textures\\TEST.png", sf::Vector2u(32, 32), level, mapWidth, mapHeight)){
+    if(!map.load("./Textures/TEST.png", sf::Vector2u(32, 32), level, mapWidth, mapHeight)){
         return 1;
 
     };
@@ -72,16 +74,18 @@ int main()
     // create the four different faces of the sprite
     sf::IntRect rightFace(0, 0, 32, 32);
     sf::IntRect leftFace(32, 0, 32, 32);
-    sf::IntRect towardsFace(0, 32, 32, 64);
-    sf::IntRect awayFace(32, 32, 64, 64);
+    sf::IntRect downFace(32, 32, 32, 32);
+    sf::IntRect upFace(0, 32, 32, 32);
 
 
     // Initialize the Player - add texture and origin - and event
     // look into sf::Event() - I don't think that needs to be there but for now it breaks without it
-    Player Player(texture, rightFace, {mapSize.x / 2.0f, mapSize.y / 2.0f}, sf::Event());
+    Player Player(texture, downFace, {mapSize.x / 2.0f, mapSize.y / 2.0f}, sf::Event());
     
     //resize the player sprite
     Player.sprite.setScale(2, 2);
+
+    sf::Clock timer;
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -103,58 +107,53 @@ int main()
                     fullscreen = !fullscreen;
                     // if true - go to fullscreen - else go to windowed mode
                     if (fullscreen) {
-                        window.create(sf::VideoMode::getDesktopMode(), "Test", sf::Style::Fullscreen);
+                        window.create(sf::VideoMode::getDesktopMode(), "Adventures of Narnok", sf::Style::Fullscreen);
                     }
                     else {
-                        window.create(sf::VideoMode(800, 600), "Test", sf::Style::Default);
+                        window.create(sf::VideoMode(800, 600), "Adventures of Narnok", sf::Style::Default);
                     }
 
                     // Update the view size and center whenever switching from windowed to fullscreen - keeps everything consistent
                     view.setSize(window.getSize().x, window.getSize().y);
                     view.setCenter(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
                     window.setView(view);
-
                 }
             }
 
         }
-
-
-        // Declare and initialize the sprite variable
-        sf::Sprite sprite;
         
         // clear the window 
         window.clear();
 
-         // draw the map
+        // draw the map
         window.draw(map);
         
         // initialize the direction 
         sf::Vector2f direction = {0.f, 0.f};
+        
+        sf::Time passed = timer.restart();
 
         // for some reason if statements work better than switch case????
         // depending on keypressed - changes direction in the x or y direction to monitor's boundary
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-                // this breaks my view - Player.sprite.setTextureRect(towardsFace);
-                // I don't know why - for now I will comment this out
-                direction.y -= 2;
+                Player.sprite.setTextureRect(upFace);
+                direction.y -= 4 * map.getTileSize().y * passed.asSeconds();
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-                // similar problem as above
-                // Player.sprite.setTextureRect(awayFace);
-                direction.y += 2;
+                Player.sprite.setTextureRect(downFace);
+                direction.y += 4 * map.getTileSize().y * passed.asSeconds();
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
                 Player.sprite.setTextureRect(leftFace);
-                direction.x -= 2;
+                direction.x -= 4 * map.getTileSize().x * passed.asSeconds();
             
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
                 Player.sprite.setTextureRect(rightFace);
-                direction.x += 2;
+                direction.x += 4 * map.getTileSize().x * passed.asSeconds();
         }
 
         // update the position of the sprite based on boundaries of map
